c729f18f6533a19acb6c12dcad1d5f5bd8bf2f78
Thomas Raducha <thomasraducha@gmail.com>
2023-02-28 20:47:45 +0100

    add routes

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations' }
   get '/member-data', to: 'members#show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :articles
+  resources :users
   # Defines the root path route ("/")
   # root "articles#index"
 end
